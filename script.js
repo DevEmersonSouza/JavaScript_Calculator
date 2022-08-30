02925400ab100aec145f9addee5ffefbe6653828
Emerson Souza <souzaemerson003@gmail.com>
2022-08-30 00:02:38 -0300

    mudancas no CSS e inicio do JS

---
+++ script.js
@@ -0,0 +1,13 @@
+let display = "";
+let fatorNumerico = 0;
+let operacao = "";
+
+function numberSeven() {
+    document.getElementById("calcDisplay").value = 7;
+}
+function numberEight() {
+    document.getElementById("calcDisplay").value = 8;
+}
+function adcNumber() {
+    let numAtual = display
+}
